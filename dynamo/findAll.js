a53d6075b17995c5ffa823bccd36723cdd10ab65
todinesh <33295255+todinesh@users.noreply.github.com>
2019-01-25 15:07:03 -0500

    Revert "Initial commit"
    This reverts commit ee00713abd14e43d3f7496415742ae6f1b525ce2.

--- dynamo/findAll.js
+++
@@ -1,6 +0,0 @@
-const AWS = require('aws-sdk');
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-module.exports = () => dynamoDb.scan({ TableName: process.env.DYNAMODB_TABLE })
-    .promise()
-    .then(r => r.Items);  
